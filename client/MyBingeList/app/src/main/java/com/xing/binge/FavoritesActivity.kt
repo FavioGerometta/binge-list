67cf3e27d582ce25f330356d5cb1f06a671fea58
Júlia Garrigós <>
2019-02-12 16:25:39 +0100

    Android project template added

---
+++ client/MyBingeList/app/src/main/java/com/xing/binge/FavoritesActivity.kt
@@ -0,0 +1,15 @@
+package com.xing.binge
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class FavoritesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorites)
+
+        findViewById<TextView>(R.id.textView_movieList_callToAction).setText(R.string.favorites_empty_place_holder)
+    }
+}
